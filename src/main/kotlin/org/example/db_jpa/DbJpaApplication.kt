cfce8790a92cc357c9a11c60252869557c37997f
Emanuelle Schiavon <emanuelleschiavon@gmail.com>
2024-07-12 12:21:12 -0300

    Initializer template

---
+++ src/main/kotlin/org/example/db_jpa/DbJpaApplication.kt
@@ -0,0 +1,11 @@
+package org.example.db_jpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbJpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<DbJpaApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/db_jpa/DbJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.db_jpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DbJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
